47cbcec3607e58950753181c3f53b9e807d59949
GrechkaFromUA <makskuts99@gmail.com>
2017-05-02 19:15:35 +0300

    Db v 001

--- app/src/main/java/maximprytyka/com/timetable/DBHelper.java
+++ app/src/main/java/maximprytyka/com/timetable/DBHelper.java
@@ -1,9 +1,39 @@
 package maximprytyka.com.timetable;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by Максим on 02.05.2017.
- */
+public class DBHelper  extends SQLiteOpenHelper{
 
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "TimeTableDB";
+    public static final String TABLE_TIME = "time";
+    public static final String TABLE_SUB = "subjects";
+    public static final String TABLE_TEACHERS = "teachers";
+    public static final String TABLE_BUILDINGS = "buikdings";
+    public static final String TABLE_TYPE = "lesson type";
+
+    public static final String KEY_ID = "_id";
+    public static final String KEY_VALUE = "value";
+    public static final String KEY_VALUE_END = "value end";
+
+
+
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+
+    }
 }
--- app/src/main/java/maximprytyka/com/timetable/Fragmets/TeachersFragment.java
+++ app/src/main/java/maximprytyka/com/timetable/Fragmets/TeachersFragment.java
@@ -4,15 +4,19 @@ package maximprytyka.com.timetable.Fragmets;
 import android.annotation.TargetApi;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import maximprytyka.com.timetable.DBHelper;
 import maximprytyka.com.timetable.R;
 
 
@@ -22,10 +26,42 @@ public class TeachersFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_teachers,container,false);
+        View v = inflater.inflate(R.layout.fragment_example,container,false);
         final FragmentManager fm = getFragmentManager();
         final FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab);
         fab.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.edit));
+
+
+        //SQlite Components
+        DBHelper dbHelper = new DBHelper(getActivity());
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        db.execSQL("create table if not exists "+dbHelper.TABLE_TEACHERS+" ("+dbHelper.KEY_ID+" integer primary key,"+dbHelper.KEY_VALUE+"text)");
+
+
+
+
+        //put From DB
+        Cursor cursor = db.query(DBHelper.TABLE_TEACHERS, null, null, null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+            int nameIndex = cursor.getColumnIndex(DBHelper.KEY_VALUE);
+            do {
+                Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
+                        ", value = " + cursor.getString(nameIndex));
+            } while (cursor.moveToNext());
+        } else
+            Log.d("mLog","0 rows");
+
+        cursor.close();
+
+
+
+
+
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
